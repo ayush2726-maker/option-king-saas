84d768c7776025fa5a071402246f52302a367646
ayush2726-maker <ayush2726@gmail.com>
2026-08-28 10:57:54 +0530

    Mount Alexa OAuth account linking routes

--- alexa_app.py
+++ alexa_app.py
@@ -1,4 +1,6 @@
 from main import app
 from alexa_option_king.multiuser_routes import router as alexa_router
+from alexa_option_king.oauth_routes import router as alexa_oauth_router
 
 app.include_router(alexa_router)
+app.include_router(alexa_oauth_router)
