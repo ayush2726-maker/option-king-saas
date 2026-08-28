0c4b83055b2bfcfa4aa3ca3af4fbfa7752afe413
ayush2726-maker <ayush2726@gmail.com>
2026-08-28 11:51:50 +0530

    Expose Option King Alexa privacy policy

--- alexa_app.py
+++ alexa_app.py
@@ -1,6 +1,8 @@
 from main import app
 from alexa_option_king.multiuser_routes import router as alexa_router
 from alexa_option_king.oauth_routes import router as alexa_oauth_router
+from alexa_option_king.privacy_routes import router as privacy_router
 
 app.include_router(alexa_router)
 app.include_router(alexa_oauth_router)
+app.include_router(privacy_router)
