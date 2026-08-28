1cfbf3b5da4bc5556a7b2c5c455a1236b71baf85
ayush2726-maker <ayush2726@gmail.com>
2026-08-28 10:32:01 +0530

    Route Alexa through multi-user resolver

--- alexa_app.py
+++ alexa_app.py
@@ -1,4 +1,4 @@
 from main import app
-from alexa_option_king.routes import router as alexa_router
+from alexa_option_king.multiuser_routes import router as alexa_router
 
 app.include_router(alexa_router)
