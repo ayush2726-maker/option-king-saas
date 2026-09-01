d8045528c035ed425c090baf5632a12535d3d655
ayush2726-maker <ayush2726@gmail.com>
2026-09-01 13:06:40 +0530

    Route LIVE home dashboard from Angel broker truth

--- alexa_app.py
+++ alexa_app.py
@@ -5,8 +5,10 @@ from main import app
 import bot.live_gateway_db_bridge_v1  # noqa: F401
 import bot.live_quality_fix_v1  # noqa: F401
 from bot.live_mode_broker_truth_middleware import install as install_live_broker_truth
+from bot.live_signal_broker_truth_middleware import install as install_live_signal_broker_truth
 
 install_live_broker_truth(app)
+install_live_signal_broker_truth(app)
 
 from alexa_option_king.multiuser_routes import router as alexa_router
 from alexa_option_king.oauth_routes import router as alexa_oauth_router
